09e6c3597271d1c10358d5d282970dbbee4254ea
Harsh Suthar <143863260+SutharHarsh@users.noreply.github.com>
2024-07-25 00:07:14 +0530

    Update evm-wallet.ts

--- evm-wallet.ts
+++ evm-wallet.ts
@@ -1,8 +1,16 @@
 import { EVMWallet } from "@catalogfi/wallets";
 import { JsonRpcProvider, Wallet } from "ethers";
 
+
 const provider = new JsonRpcProvider("https://rpc.ankr.com/eth");
-const privateKey = "YOUR PRIVATE KEY";
+
+
+const privateKey = "YOUR PRIVATE KEY"; 
+
+
 const wallet = new Wallet(privateKey, provider);
 
+
 const evmWallet = new EVMWallet(wallet);
+
+console.log("EVM Wallet Address:", evmWallet.getAddress());
